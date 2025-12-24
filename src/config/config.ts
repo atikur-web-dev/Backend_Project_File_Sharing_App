5c560dc4349b16d135f0779e78b2d81b4f3ebcdb
Atikur Rahman <atikurrahman160313@gmail.com>
2025-12-24 21:09:23 +0600

    fix: update default port in config to 4000

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const _config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
 };
 
 export const config = Object.freeze(_config);
