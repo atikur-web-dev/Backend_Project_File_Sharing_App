2deefc4341df8249e3ad861f9b7aa3dd95acb622
Atikur Rahman <atikurrahman160313@gmail.com>
2025-12-24 21:29:20 +0600

    fix: correct APP_URL construction in config and update server log message

--- server.ts
+++ server.ts
@@ -3,5 +3,5 @@ import app from './App.ts';
 import { config } from './src/config/config.ts'; // path check
 
 app.listen(config.port, () => {
-  log(`Server is running on http://localhost:${config.port}`);
-});
+  log(`Server is running on http://localhost:${config.APP_URL}`);
+ });
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const _config = {
   port: process.env.PORT || 4000,
+  APP_URL: `${process.env.APP_URL}:${process.env.PORT}` || `http://localhost:4000 : ${process.env.PORT}`,
 };
 
 export const config = Object.freeze(_config);
